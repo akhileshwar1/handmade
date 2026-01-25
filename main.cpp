be0b037c849e2dbd33884917330ec5d513881953
Akhil Kandi <akhilprm999@gmail.com>
2026-01-25 19:37:52 +0530

    Add ALSA sound support to the platform layer

--- main.cpp
+++ main.cpp
@@ -42,44 +42,51 @@ void renderweirdgradient(X_offscreen_buffer *buffer) {
     }
 }
 
-void sine_wave_sound(short *buffer, size_t sample_count, int freq) {
-    for (int i = 0; i < sample_count; i++) {
-        buffer[i] = 10000 * sinf(2*M_PI*freq*((float)i/SAMPLE_RATE));
-    }
-}
-
-void square_wave_sound(short *buffer, size_t sample_count) {
-    short value = 16000;
-    int period = 0;
-    for (int i = 0; i < sample_count; i++) {
-        if (period == 10) {
-            period = 0;
-            value = -value;
-        }
-        buffer[i] = value;
-    }
-}
-
-void XPlaySound(short *samples_buffer) {
+snd_pcm_t *XInitSound() {
     snd_pcm_t *pcm;
-    snd_pcm_open(&pcm, "default", SND_PCM_STREAM_PLAYBACK, 0);
-
+    snd_pcm_state_t state;
+    int dir = 0; // basically chose a value almost equal to what was passed.
+    int channels = 2;
+    static unsigned int period_time = 100000;       /* period time in us */
+    int err;
+    err = snd_pcm_open(&pcm, "hw:PCH,0", SND_PCM_STREAM_PLAYBACK, 0);
+    if (err < 0) {
+        printf("failed at %s\n", snd_strerror(err));
+    }
+    state = snd_pcm_state(pcm);
     snd_pcm_hw_params_t *hw_params;
     snd_pcm_hw_params_alloca(&hw_params);
     snd_pcm_hw_params_any(pcm, hw_params);
-    snd_pcm_hw_params_set_access(pcm, hw_params, SND_PCM_ACCESS_RW_INTERLEAVED);
-    snd_pcm_hw_params_set_format(pcm, hw_params, SND_PCM_FORMAT_S16_LE);
-    snd_pcm_hw_params_set_channels(pcm, hw_params, 1);
-    snd_pcm_hw_params_set_rate(pcm, hw_params, 48000, 0);
-    snd_pcm_hw_params_set_periods(pcm, hw_params, 10, 0);
-    snd_pcm_hw_params_set_period_time(pcm, hw_params, 100000, 0); // 0.1 seconds
+    err = snd_pcm_hw_params_set_access(pcm, hw_params, SND_PCM_ACCESS_MMAP_INTERLEAVED);
+    if (err < 0) {
+        printf("failed at %s\n", snd_strerror(err));
+    }
+    err = snd_pcm_hw_params_set_format(pcm, hw_params, SND_PCM_FORMAT_S16_LE);
+    if (err < 0) {
+        printf("failed at %s\n", snd_strerror(err));
+    }
+    err = snd_pcm_hw_params_set_channels(pcm, hw_params, channels);
+    if (err < 0) {
+        printf("failed at %s\n", snd_strerror(err));
+    }
+    err = snd_pcm_hw_params_set_rate(pcm, hw_params, 48000, 0);
+    if (err < 0) {
+        printf("failed at %s\n", snd_strerror(err));
+    }
+    err = snd_pcm_hw_params_set_buffer_size(pcm, hw_params, 4096);
+    if (err < 0) {
+        printf("failed at %s\n", snd_strerror(err));
+    }
+    err = snd_pcm_hw_params_set_period_time_near(pcm, hw_params, &period_time, &dir);
+    if (err < 0) {
+        printf("failed at %s\n", snd_strerror(err));
+    }
+    
     snd_pcm_hw_params(pcm, hw_params);
+    state = snd_pcm_state(pcm);
+    snd_pcm_prepare(pcm);
 
-    // write the samples.
-    snd_pcm_writei(pcm, samples_buffer, 48000);
-    // wait for the device to play all our samples.
-    snd_pcm_drain(pcm);
-    snd_pcm_close(pcm);
+    return pcm;
 }
 
 void XUpdateBufferDims(Display *display, Window window, X_offscreen_buffer *buffer) {
@@ -174,10 +181,7 @@ int main() {
     XGrabKeyboard(display, window, False, GrabModeAsync, GrabModeAsync, CurrentTime);
     XAutoRepeatOn(display); 
 
-    short samples_buffer[SAMPLE_RATE];
-    // sine_wave_sound(&samples_buffer[0], SAMPLE_RATE, 200);
-    square_wave_sound(&samples_buffer[0], SAMPLE_RATE);
-    XPlaySound(samples_buffer);
+    snd_pcm_t *pcm = XInitSound();
     while (Running) {
         while (XPending(display)) {
             XEvent event;
@@ -190,6 +194,30 @@ int main() {
         renderweirdgradient(&buffer);
         XDisplayBufferInWindow(display, window, gc, &buffer);
         buffer.XOffset++;
+
+        snd_pcm_sframes_t available = snd_pcm_avail_update(pcm); 
+        if (available < 0) {
+            printf ("availability error");
+        } 
+        snd_pcm_uframes_t offset;
+        snd_pcm_uframes_t frames;
+        const snd_pcm_channel_area_t *areas;
+        frames = 1;
+        int can_write = snd_pcm_mmap_begin(pcm, &areas, &offset, &frames);
+        unsigned short *addr = (unsigned short *)areas[0].addr;
+        addr += offset * 2; // mutliply by 2 because the offset is in steps = 2*short sample value.
+        if (can_write < 0) {
+            printf("can't write %s\n", snd_strerror(can_write));
+        }
+        /*
+         [L0 R0] [L1 R1] where l and r are 16 bits each.
+         and they mean left and right speaker, 2 samples per frame.
+        */
+        for (int i = 0; i < 2; i++) {
+            short value = 16000;
+            *addr++ = value;
+        }
+        snd_pcm_mmap_commit(pcm, offset, 1);
         // buffer.YOffset++;
     }
 
