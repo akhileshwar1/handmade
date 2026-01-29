07f30ee88db08cc19c18655afcf05bbef767d59d
Akhil Kandi <akhilprm999@gmail.com>
2026-01-29 17:31:39 +0530

    Fix and refactor the sound code

--- main.cpp
+++ main.cpp
@@ -11,6 +11,7 @@ typedef uint8_t uint8;
 typedef uint16_t uint16;
 typedef uint32_t uint32;
 typedef uint64_t uint64;
+typedef int16_t int16;
 typedef float real32;
 typedef double real64;
 
@@ -27,7 +28,14 @@ typedef struct {
     int depth; 
 } X_offscreen_buffer;
 
-X_offscreen_buffer buffer = {};
+typedef struct {
+    snd_pcm_t *pcm;
+    real32 sample_rate; // we can't produce a continous wave, so we snapshot aka sample it.
+    real32 amplitude;
+    real32 frequency; // full cycles per second.
+    real32 phase;
+    real32 phase_increment;
+} X_sound_config;
 
 void renderweirdgradient(X_offscreen_buffer *buffer) {
     buffer->data = (uint32 *)malloc(buffer->window_width*buffer->window_height*4); // 4 bytes for a pixel RR GG BB XX
@@ -42,51 +50,60 @@ void renderweirdgradient(X_offscreen_buffer *buffer) {
     }
 }
 
-snd_pcm_t *XInitSound() {
-    snd_pcm_t *pcm;
+int XInitSound(X_sound_config *sound_config) {
     snd_pcm_state_t state;
     int dir = 0; // basically chose a value almost equal to what was passed.
     int channels = 2;
     static unsigned int period_time = 100000;       /* period time in us */
     int err;
-    err = snd_pcm_open(&pcm, "hw:PCH,0", SND_PCM_STREAM_PLAYBACK, 0);
+    err = snd_pcm_open(&sound_config->pcm, "hw:0,0", SND_PCM_STREAM_PLAYBACK, 0);
     if (err < 0) {
         printf("failed at %s\n", snd_strerror(err));
     }
-    state = snd_pcm_state(pcm);
+    state = snd_pcm_state(sound_config->pcm);
     snd_pcm_hw_params_t *hw_params;
     snd_pcm_hw_params_alloca(&hw_params);
-    snd_pcm_hw_params_any(pcm, hw_params);
-    err = snd_pcm_hw_params_set_access(pcm, hw_params, SND_PCM_ACCESS_MMAP_INTERLEAVED);
+    snd_pcm_hw_params_any(sound_config->pcm, hw_params);
+    err = snd_pcm_hw_params_set_access(sound_config->pcm, hw_params, SND_PCM_ACCESS_MMAP_INTERLEAVED);
     if (err < 0) {
         printf("failed at %s\n", snd_strerror(err));
     }
-    err = snd_pcm_hw_params_set_format(pcm, hw_params, SND_PCM_FORMAT_S16_LE);
+    err = snd_pcm_hw_params_set_format(sound_config->pcm, hw_params, SND_PCM_FORMAT_S16_LE);
     if (err < 0) {
         printf("failed at %s\n", snd_strerror(err));
     }
-    err = snd_pcm_hw_params_set_channels(pcm, hw_params, channels);
+    err = snd_pcm_hw_params_set_channels(sound_config->pcm, hw_params, channels);
     if (err < 0) {
         printf("failed at %s\n", snd_strerror(err));
     }
-    err = snd_pcm_hw_params_set_rate(pcm, hw_params, 48000, 0);
+    err = snd_pcm_hw_params_set_rate(sound_config->pcm, hw_params, 48000, 0);
     if (err < 0) {
         printf("failed at %s\n", snd_strerror(err));
     }
-    err = snd_pcm_hw_params_set_buffer_size(pcm, hw_params, 4096);
+    // add one sec worth of buffer size i.e 48000 frames.
+    // so that this one sec is always maintained between read and write pointer
+    // of the audio ring buffer.
+    err = snd_pcm_hw_params_set_buffer_size(sound_config->pcm, hw_params, 48000);
     if (err < 0) {
         printf("failed at %s\n", snd_strerror(err));
     }
-    err = snd_pcm_hw_params_set_period_time_near(pcm, hw_params, &period_time, &dir);
+    err = snd_pcm_hw_params_set_period_time_near(sound_config->pcm, hw_params, &period_time, &dir);
     if (err < 0) {
         printf("failed at %s\n", snd_strerror(err));
     }
-    
-    snd_pcm_hw_params(pcm, hw_params);
-    state = snd_pcm_state(pcm);
-    snd_pcm_prepare(pcm);
+    // 48 period size * 1000 (100ms period time) == 1 second worth of buffer.
+    // period is the smallest unit of work read from sound buffer.
+    err = snd_pcm_hw_params_set_period_size(sound_config->pcm, hw_params, 48,0);
+    if (err < 0) {
+        printf("failed at %s\n", snd_strerror(err));
+    }
 
-    return pcm;
+    err = snd_pcm_hw_params(sound_config->pcm, hw_params);
+    if (err < 0) {
+        printf("failed at %s\n", snd_strerror(err));
+    }
+    state = snd_pcm_state(sound_config->pcm);
+    return err;
 }
 
 void XUpdateBufferDims(Display *display, Window window, X_offscreen_buffer *buffer) {
@@ -145,8 +162,62 @@ void handleEvent(Display *display, Window window, GC gc, XEvent event,
     }
 }
 
+int XFillSoundBuffer(X_sound_config *sound_config) {
+    snd_pcm_sframes_t available = snd_pcm_avail_update(sound_config->pcm); 
+    if (available < 0) {
+        printf ("availability error %s\n", snd_strerror(available));
+        snd_pcm_recover(sound_config->pcm, available, 0);
+        available = snd_pcm_avail_update(sound_config->pcm);
+    }
+    snd_pcm_uframes_t offset;
+    snd_pcm_uframes_t frames;
+    const snd_pcm_channel_area_t *areas;
+    frames = available;
+    int can_write = snd_pcm_mmap_begin(sound_config->pcm, &areas, &offset, &frames);
+    if (can_write < 0) {
+        printf("can't write %s\n", snd_strerror(can_write));
+        return -1;
+    }
+    /*
+         [L0 R0] [L1 R1] where l and r are 16 bits each.
+         and they mean left and right speaker, 2 samples per frame.
+        */
+
+    int16 *sample_ptr = (int16 *)((uint8 *)areas[0].addr + (offset * areas[0].step / 8));
+      
+    for (uint32 i = 0; i < frames; i++) {
+        int16 sample_value = (int16)(sound_config->amplitude * sinf(sound_config->phase));
+        *sample_ptr++ = sample_value; // LEFT
+        *sample_ptr++ = sample_value; // RIGHT
+
+        sound_config->phase += sound_config->phase_increment;
+
+        if (sound_config->phase >= 2.0f * M_PI) {
+            sound_config->phase -= 2.0f * M_PI;
+        }
+
+    }
+    snd_pcm_mmap_commit(sound_config->pcm, offset, frames);
+    snd_pcm_state_t state = snd_pcm_state(sound_config->pcm);   
+    if (state == SND_PCM_STATE_PREPARED) {
+        int err = snd_pcm_start(sound_config->pcm);
+        if (err < 0) {
+            printf("failed at uhuh%s\n", snd_strerror(err));
+        }
+    }
+    return 0;
+}
+
 
 int main() {
+    X_sound_config sound_config = {};
+    X_offscreen_buffer buffer = {};
+    sound_config.sample_rate = 48000.0f;
+    sound_config.amplitude = 10000.0f;
+    sound_config.frequency = 440.0f;
+    sound_config.phase = 0.0f;
+    sound_config.phase_increment = (2.0f * M_PI * sound_config.frequency) / sound_config.sample_rate;
+
     Display *display = XOpenDisplay(NULL);
     if (!display) {
         printf("Display not found\n");
@@ -181,7 +252,7 @@ int main() {
     XGrabKeyboard(display, window, False, GrabModeAsync, GrabModeAsync, CurrentTime);
     XAutoRepeatOn(display); 
 
-    snd_pcm_t *pcm = XInitSound();
+    int err = XInitSound(&sound_config);
     while (Running) {
         while (XPending(display)) {
             XEvent event;
@@ -194,30 +265,11 @@ int main() {
         renderweirdgradient(&buffer);
         XDisplayBufferInWindow(display, window, gc, &buffer);
         buffer.XOffset++;
+        err = XFillSoundBuffer(&sound_config);
+        if (err < 0) {
+            printf("Sound error \n");
+        }
 
-        snd_pcm_sframes_t available = snd_pcm_avail_update(pcm); 
-        if (available < 0) {
-            printf ("availability error");
-        } 
-        snd_pcm_uframes_t offset;
-        snd_pcm_uframes_t frames;
-        const snd_pcm_channel_area_t *areas;
-        frames = 1;
-        int can_write = snd_pcm_mmap_begin(pcm, &areas, &offset, &frames);
-        unsigned short *addr = (unsigned short *)areas[0].addr;
-        addr += offset * 2; // mutliply by 2 because the offset is in steps = 2*short sample value.
-        if (can_write < 0) {
-            printf("can't write %s\n", snd_strerror(can_write));
-        }
-        /*
-         [L0 R0] [L1 R1] where l and r are 16 bits each.
-         and they mean left and right speaker, 2 samples per frame.
-        */
-        for (int i = 0; i < 2; i++) {
-            short value = 16000;
-            *addr++ = value;
-        }
-        snd_pcm_mmap_commit(pcm, offset, 1);
         // buffer.YOffset++;
     }
 
